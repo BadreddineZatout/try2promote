0ba68abfb90b8bb9f2ada9799692804f48fa0e4f
BadreddineZatout <hb_zatout@esi.dz>
2022-07-18 22:34:03 +0100

    booking

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Services from "./components/Services";
+import Booking from "./components/Booking";
 
 function App() {
   return (
     <>
       <Header />
       <Services />
+      <Booking />
     </>
   );
 }
---
+++ frontend/src/components/Booking.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Booking() {
+  return (
+    <div id="booking" className="mb-24 flex items-center justify-center">
+      <div className="flex w-3/4 flex-col justify-center font-rambla text-3xl font-semibold">
+        <p className="text-center">We Care about you, Lazero puts you first!</p>
+        <p className="text-center">
+          Try our servicec and judge by yoursel, book a session with us!
+        </p>
+        <button className="mx-auto mt-5 flex-none bg-Jasmine px-10 py-3 font-rambla text-lg">
+          book now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Booking;
