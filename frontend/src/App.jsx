5916fdd18d5c0244f84ae7ea40a7e3d885ee6bc3
BadreddineZatout <hb_zatout@esi.dz>
2022-07-18 22:50:54 +0100

    wip

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Services from "./components/Services";
 import Booking from "./components/Booking";
+import Blog from "./components/Blog";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Services />
       <Booking />
+      <Blog />
     </>
   );
 }
---
+++ frontend/src/components/Blog.jsx
@@ -0,0 +1,16 @@
+import BlogCard from "./BlogCard";
+
+function Blog() {
+  return (
+    <div>
+      <h1>Read our latest blog posts</h1>
+      <div>
+        <BlogCard />
+        <BlogCard />
+        <BlogCard />
+      </div>
+    </div>
+  );
+}
+
+export default Blog;
